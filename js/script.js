032bf8073a4511d1ef27b7b61d6b8e88d7321b6e
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-12-14 22:31:37 -0300

    Imagens

---
+++ js/script.js
@@ -0,0 +1,13 @@
+(function(){
+    var cnv = document.querySelector("convas");
+    var ctx = cnv.getContext("2d");
+
+    function update(){}
+
+    function render(){}
+
+    function loop(){
+        update();
+        loop();
+    }
+}());
