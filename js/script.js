59a44f1a1f2c19545f7bbcb680184724569b322d
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-12-17 14:57:44 -0300

    Movimentação

--- js/script.js
+++ js/script.js
@@ -2,9 +2,24 @@
 	//elemento canvas e contexto de renderização
 	var cnv = document.querySelector("canvas");
 	var ctx = cnv.getContext("2d");
+
+	var WIDTH = cnv;
+
+	//movimentação do personagem
+	var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40;
+	var mvLeft = mvUp = mvRight = mvDown = false;
 	
 	//tamanho dos blocos
 	var tileSize = 32;
+
+	//Personagem
+	var player = {
+		x: tileSize + 2,
+		y: tileSize + 2,
+		width: 28,
+		height: 28,
+		speed: 2
+	};
 	
 	//mapa do labirinto
 	var maze = [
@@ -30,11 +45,64 @@
 		[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 	];
 
+	window.addEventListener("keydown", keydownHandler, false);
+	window.addEventListener("keyup", keyupHandler, false);
+
+	function keydownHandler(e){
+		var key = e.keyCode;
+		switch(key){
+			case LEFT:
+				mvLeft = true;
+				break;
+			case UP:
+				mvUp = true;
+				break;
+			case RIGHT:
+				mvRight = true;
+				break;
+			case DOWN:
+				mvDown = true;
+				break;		
+		}
+	}
+
+	function keyupHandler(e){
+		var key = e.keyCode;
+		switch(key){
+			case LEFT:
+				mvLeft = false;
+				break;
+			case UP:
+				mvUp = false;
+				break;
+			case RIGHT:
+				mvRight = false;
+				break;
+			case DOWN:
+				mvDown = false;
+				break;		
+		}
+	}
+
     //atualização cíclica do programa
-	function update(){}
+	function update(){
+		if(mvLeft && !mvRight){
+			player.x -= player.speed;
+		}else
+		if(mvRight && !mvLeft){
+			player.x += player.speed;
+		}
+		if(mvUp && !mvDown){
+			player.y -= player.speed;
+		}else
+		if(mvDown && !mvUp){
+			player.y += player.speed;
+		}
+	}
 	
 	//renderização (desenha na tela)
 	function render(){
+		ctx.clearRect();
         ctx.save();
 		//procedimento que varre as linhas e colunas do labirinto
 		for(var row in maze){
@@ -54,7 +122,7 @@
 
         //personagem
         ctx.fillStyle = "#00f";
-        ctx.fillRect(34,34,28,28);
+        ctx.fillRect(player.x,player.y,player.width,player.height);
         ctx.restore();
 	}
 	
